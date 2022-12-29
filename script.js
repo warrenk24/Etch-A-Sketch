772867a10b08396b6e9b83e01c0da38662f60445
Khris <kaw0115@auburn.edu>
2022-12-29 13:34:34 -0600

    add a change size function to allow user to change grid size via input field

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-const gridGenerator = (num) => {
+const gridGenerator = (gridSize) => {
   const container = document.querySelector(".grid-container");
-  container.style.gridTemplateRows = 'repeat(16, 1fr)'
-  container.style.gridTemplateColumns = 'repeat(16, 1fr)'
+  let gridItem = container.querySelectorAll('div')
+  gridItem.forEach(div => {
+    div.remove()
+  })
+  container.style.gridTemplateRows =  `repeat(${gridSize}, 1fr)`
+  container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
+  let size = gridSize* gridSize
 
-  for (i = 0; i < num; i++) {
+  for (i = 0; i < size; i++) {
     let newSquare = document.createElement("div");
-    newSquare.style.border = '1px solid red'
+    newSquare.style.border = '1px solid black'
     newSquare.classList.add("grid-item");
     container.insertAdjacentElement('beforeend', newSquare);
   }
 };
 
-gridGenerator(256);
+const changeSize = input => {
+    gridGenerator(input)
+};
 
